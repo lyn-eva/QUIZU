93bd3a16caa5e248a6c583ba16509e0f594a9c0f
Zeiyar Lynn <zeiyarlynn@gmail.com>
2021-11-02 23:37:53 +0630

    proceeded to final phase

--- JS/main-page.js
+++ JS/main-page.js
@@ -1,63 +1,73 @@
 const questionID = document.getElementById('question').lastElementChild;
-const choice_text = Array.from(document.getElementsByClassName('choice-text'));
-const choices = Array.from(document.getElementsByClassName('choice'));
+const choices = document.getElementById('choices');
+const boolean = document.getElementById('boolean');
+
 const current_no = document.getElementById('no.');
 const bar = document.getElementById('bar');
 const points = document.getElementById('point');
 const final = document.getElementById('omedetou');
+
 // parse localstorage
 let q_set = JSON.parse(localStorage.getItem('questions'));
 let questions = q_set["results"];
-console.log(questions);
-
-// eve
-choices.forEach( choice => {
-   choice.addEventListener('click', e => {
-      let index = q_no, clc = 'false', len = questions.length;
-      const temp = e.target.lastElementChild.innerHTML;
-
-      if (temp == questions[index-1]["correct_answer"]) {
-         clc = true;   
-         points.innerHTML = parseInt(points.innerHTML) + 10; // this is legit 0-0-
-      } 
-
-      e.target.classList.add(clc);
-      setTimeout(() => {
-         e.target.classList.remove(clc);
-         if (index < len)
-            nextQuestion(questions[index]);
-         else 
-            omedetou(points.innerHTML, len);
-      }, 700);
-   })
-})
 
 let q_no = 0, max_q = questions.length;
 startGame();
 
-// func
 function startGame() {// dont ne"d
-   nextQuestion(questions[q_no])
+   nextQuestion(questions[q_no]);
 }
 
 // next question
 function nextQuestion(questObj) {
    questionID.innerHTML = questObj["question"]
-   if (questObj["type"] == "multiple") {
-      mcq(questObj)
-   }
-   q_no++;
-   current_no.innerHTML = `${q_no} / ${max_q}`;
-   bar.style.width = `${(q_no/max_q) * 100}%`;
+   generateQuestion(questObj);
+   current_no.innerHTML = `${q_no+1} / ${max_q}`;
+   bar.style.width = `${((q_no+1)/max_q) * 100}%`;
 }
 
-// mcq
-function mcq(obj) {
+function generateQuestion(obj) {
    let choices_array = obj["incorrect_answers"].concat(obj["correct_answer"])
    choices_array = shuffle(choices_array)
-   choices.forEach( (iter, i) => {
-      iter.lastElementChild.innerHTML = choices_array[i];
-   })
+
+   choices_array.forEach( iter => {
+      let createDiv = document.createElement('div');
+      createDiv.classList.add("choice");
+      if (obj.type == 'multiple') {
+         createDiv.innerHTML = `<span>A</span><p class="choice-text">${iter}</p>`
+      }
+      else {
+         createDiv.classList.add("bool");
+         createDiv.innerHTML = `<p class="choice-text">${iter}</p>`
+      }
+         
+      choices.appendChild(createDiv);
+      createDiv.addEventListener('click', settle);
+   });
+}
+
+function settle(e) {
+   let clc = 'false';
+   const temp = e.target.lastElementChild.innerHTML;
+
+   if (temp == questions[q_no]["correct_answer"]) {
+      clc = 'true';   
+      points.innerHTML = parseInt(points.innerHTML) + 10; // this is legit 0-0-
+   } 
+
+   e.target.classList.add(clc);
+   setTimeout(() => {
+      e.target.classList.remove(clc);
+      q_no++;
+      
+      while(choices.firstChild)
+         choices.removeChild(choices.firstChild);
+
+      if (q_no < questions.length)
+         nextQuestion(questions[q_no]);
+      else 
+         omedetou(points.innerHTML, questions.length);
+   }, 700);
 }
 
 function shuffle(que) {
